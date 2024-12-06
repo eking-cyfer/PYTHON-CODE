20e4b847d83f4a33776b9bfb16ba8a275867fe1b
eking-cyfer <edwardafatsawo20@gmail.com>
2024-12-06 19:40:45 +0000

    first commit

---
+++ Getting started with programing/main.py
@@ -0,0 +1,10 @@
+#Print a sentence
+print("Welcome to the world of coding")
+#Print a number 
+print(4)
+#Next line after print 
+print("hello python \n") 
+#pass multiple arguments for printing
+print("hello",34)
+#end argument of print statement 
+print("welcome to ", end= "*" )
